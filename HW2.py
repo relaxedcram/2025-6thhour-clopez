8997f0e1dd811aabe758b8dfc8b6a83d95821f1f
carlos lopez <cl129506@student.wpsok.org>
2025-08-20 14:07:09 -0500

    HW2

--- HW2.py
+++ HW2.py
@@ -1,27 +1,29 @@
-#name carlos lopez
-#class 6th hour
-#assigment hw1
-print('hello world')#Name:
-#Class: 5th Hour
+#Name: Carlos Lopez
+#Class: 6th Hour
 #Assignment: HW2
 
 
-#1. Print "Hello World!" print('hello world')
+#1. Print "Hello World!"
+print ("hello World")
 
 #2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
-
+number=3
+dogs='i like dogs'
+cats=True
 #3. Print all three variables on the same print function (at the same time).
-
+print (number, dogs, cats)
 #4. Create a variable that asks the user to input an integer.
-
+mouse=int(input('input a number between 1 and 10'))
 #5. Add the integer variable from #2 with the integer from #4 and print the result.
-
+number_mouse=number+mouse
+print (number_mouse)
 #6. Take the result from #5 and divide it by 2. Print the result.
-
+print(number_mouse/2)
 #7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
-
+cats=False
 #8. Print the value of the boolean variable.
-
+print(cats)
 #9. Create a variable with a number that contains decimals.
-
+money=3.67
 #10. Round the number from #9 up or down using the round function.
+print(round(money))
