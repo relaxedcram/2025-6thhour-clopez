8c27a25db2221aef5354b42d449cd4a548c492c3
carlos lopez <cl129506@student.wpsok.org>
2025-08-29 13:27:53 -0500

    HW3

---
+++ HW3.py
@@ -0,0 +1,41 @@
+#Name:Carlos L
+#Class: 6th Hour
+#Assignment: HW3
+
+#1. Print Hello World!
+print("Hello World!")
+#2. Take the variables below and change the name of the variables to match the type of
+#variable they are: string, integer, or boolean
+
+#For example, the first one, Var1 = 4, should be:
+#integerVar1 = 4 or intVar1 = 4
+
+#DO NOT CHANGE THE VALUE OF THE VARIABLE.
+
+integerVar1 = 4
+stringVar2 = "Box"
+booleanVar3 = True
+integerVar4 = "Nineteen"
+stringVar5 = "Rope"
+integerVar6 = 19
+integerVar7 = "19"
+integerVar8 = 2
+booleanVar9 = True
+booleanVar10 = False
+booleanVar11 = "True"
+booleanVar12 = "False"
+integerVar13 = 5
+stringVar14 = "The"
+stringVar15 = "Game"
+booleanVar16 = False
+integerVar17 = 3
+integerVar18 = 2
+integerVar19 = 1
+booleanVar20 = 'true'
+integerVar21 = 'false'
+integerVar22 = 'Nineteen'
+stringVar23 = 'the'
+stringVar24 = 'game'
+integerVar25 = 6
+
+#3. Take all the variables you labeled as "integer", add them together, and print the result.
