e251d2610cf52682f3259d18cf88dc3677465c3e
carlos lopez <cl129506@student.wpsok.org>
2025-09-03 13:37:44 -0500

    HW3

---
+++ HW4.py
@@ -0,0 +1,25 @@
+#Name:Carlos L
+#Class: 6th Hour
+#Assignment: HW4
+
+
+#1. Print Hello World!
+print('hello world')
+#1. Create a list with 5 strings containing 5 different names in it.
+robot_player_list=['Dog','Cats','monkey','bird','cow']
+#2. Append a new name onto the Name List.
+robot_player_list.append('zebra')
+#3. Print out the 4th name on the list.
+print(robot_player_list[3])
+#4. Create a list with 4 different integers in it.
+num_list = [6,7,5,3]
+#5. Insert a new integer into the 2nd spot and print the new list.
+print(num_list[9])
+#6. Sort the lit from lowest to highest and print the sorted list.
+num_list.sort()
+#7. Add the 1st three numbers on the sorted list together and print the sum.
+print(num_list[6] + num_list[7] + num_list[5])
+#8. Create a list with two strings, two variables, and too boolean values.
+mix_list = ["Carlos",'Aaden',2,1,True]
+#9. Create a print statement that asks the user to input their own index value for the list on #8.
+print(mix_list[int(input())])
