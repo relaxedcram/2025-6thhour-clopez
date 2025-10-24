3cac0a1ca7939ba2aafa985c22d624853e297849
carlos lopez <cl129506@student.wpsok.org>
2025-10-24 14:06:48 -0500

    HW11

--- HW11.py
+++ HW11.py
@@ -9,13 +9,22 @@
 #import random library
 import random
 #Create a temperature variable, give it a value (random from 1 to 30).
-
+random.randint(1,30)
 #Make an if statement to see if temperature variable is above 20
 #   - If yes, print it's hot
 #   - If no, bring it to next if statement
-
+a=random.randint(1,30)
+b=random.randint(1,30)
+if a>b:
+  print('its hot')
 #Make an if statement to see if temperature variable is above 10
 #   - If yes, print it's mild
 #   - If no, print it's cold
-
+a=random.randint(1,30)
+b=random.randint(1,30)
+if a>b:
+ print('its mild')
+elif a < b:
+ print('its mild')
 #Print "Thank you!" and end the code
+print('thank you')
